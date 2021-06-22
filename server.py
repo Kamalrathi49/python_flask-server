81a0b857c9666e270cc24cc244e006f64e43ce13
Kamal Rathi <kamalrathi049@gmail.com>
2021-06-22 16:19:25 +0530

    added favico in index.html

--- server.py
+++ server.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
-    return render_template('./index.html') 
+    return render_template('./index.html')
+
+
+# @app.route('/favicon.ico')
+# def favicon():
+#     return send_from_directory(os.path.join(app.root_path, 'static'),
+#                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
 
 @app.route('/2')
 def page2():
