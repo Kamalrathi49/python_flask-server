bc9ac4097a73a1a54841994a853cf8e47caf6ba9
Kamal Rathi <kamalrathi049@gmail.com>
2021-06-23 22:06:47 +0530

    added favico in index.html

--- server.py
+++ server.py
@@ -7,12 +7,6 @@ def hello_world():
     return render_template('./index.html')
 
 
-# @app.route('/favicon.ico')
-# def favicon():
-#     return send_from_directory(os.path.join(app.root_path, 'static'),
-#                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-
-@app.route('/2')
+@ app.route('/2')
 def page2():
     return "this is 2nd page"
