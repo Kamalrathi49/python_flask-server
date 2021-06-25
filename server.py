268ae434f977ecf271befa65ef1b86c0043f6669
Kamal Rathi <kamalrathi049@gmail.com>
2021-06-25 13:39:17 +0530

    url parameters

--- server.py
+++ server.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return render_template('./index.html')
+@app.route('/<username>/<int:post_id>')
+def hello_world(username=None):
+    return render_template('./index.html', name=username)
 
 
 @ app.route('/2')
