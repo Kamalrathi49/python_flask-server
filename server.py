799400cf8f53d55b0fddf8dac429891b07a08256
Kamal Rathi <kamalrathi049@gmail.com>
2021-06-25 18:09:23 +0530

    updated

--- server.py
+++ server.py
@@ -2,26 +2,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def homePage():
-    return render_template('./index.html')
+@app.route('/<string:page_name>')
+def homePage(page_name):
+    return render_template(page_name)
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    return "form submited"
 
 
-@ app.route('/about.html')
-def aboutPage():
-    return render_template('./about.html')
 
-
-@app.route('/contact.html')
-def contactPage():
-    return render_template('./contact.html')
-
-
-@app.route('/works.html')
-def worksPage():
-    return render_template('./works.html')
-
-
-@app.route('/works/work.html')
-def workPage():
-    return render_template('./work.html')
