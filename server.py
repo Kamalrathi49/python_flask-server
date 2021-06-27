57e672e0a98eb3b8d4e89e62a2cdf57375534f18
Kamal Rathi <kamalrathi049@gmail.com>
2021-06-27 15:49:53 +0530

    updated

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import csv
 app = Flask(__name__)
 
 
-@app.route('/<string:page_name>')
-def homePage(page_name):
-    return render_template(page_name)
+@app.route('/')
+def homePage():
+    return render_template("index.html")
 
 
 def store_data_txt(data):
@@ -17,12 +17,12 @@ def store_data_txt(data):
         txtfile =  database.write(f"\n[ \n   name: {name}, \n   email: {email}, \n   subject: {subject}, \n   message: {message} \n]")
 
 def store_data_csv(data):
-    with open("database.csv", 'a') as database2:
+    with open("database.csv", "a", newline="") as database2:
         name = data["name"]
         email = data["email"]
         subject = data["subject"]
         message = data["message"]
-        csvfile =  csv.writer(database2, delimiter=",", quotechar="|", quoting=csv.QUOTE_MINIMAL, newline='')
+        csvfile =  csv.writer(database2, delimiter=",", quotechar="|", quoting=csv.QUOTE_MINIMAL)
         csvfile.writerow([name, email, subject, message]) 
 
 
@@ -31,6 +31,6 @@ def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
         store_data_txt(data)
-        return "form submited"
+        return render_template("thankyou.html", thankyou="Thank you ,", msg="I will be in touch with you shortly")
     else:
-        return 'something went wrong, try again later'
+        return render_template("thankyou.html", thankyou=None, msg="'something went wrong, try again later'")
